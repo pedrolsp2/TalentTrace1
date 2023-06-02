e485e8f9f2c811b459f82f4b378001d8164fefb3
pedrolsp2 <54607320+pedrolsp2@users.noreply.github.com>
2023-06-01 21:51:59 -0300

    favorite player

--- src/pages/Components/UserProfile/index.js
+++ src/pages/Components/UserProfile/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, Image, Text, SafeAreaView, ScrollView,TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { View, Image, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 import { useNavigation, useRoute } from "@react-navigation/native";
 import firebase from "../../../Configs/firebaseconfig.js"
 import { styles } from './styles.js';
@@ -9,6 +9,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function UserProfile() {
   const route = useRoute();
+  const [like, setLike] = useState(false);
   const navigation = useNavigation();
   const storage = fb.storage();
   const [idUs, setIdUs] = useState('');
@@ -16,201 +17,206 @@ export default function UserProfile() {
   const [userData, setUserData] = useState(null);
   const [imageUrls, setImageUrls] = useState({ photoUri: null, coverUri: null });
 
-    useEffect(() => {
-      AsyncStorage.removeItem('@talenttrace:dataUsers');
-      const unsubscribe = firebase.firestore().collection('users').onSnapshot((snapshot) => {
-        const data = snapshot.docs
-          .map((doc) => {
-            const docData = doc.data();
-            const id = doc.id;
+  useEffect(() => {
+    AsyncStorage.removeItem('@talenttrace:dataUsers');
+    const unsubscribe = firebase.firestore().collection('users').onSnapshot((snapshot) => {
+      const data = snapshot.docs
+        .map((doc) => {
+          const docData = doc.data();
+          const id = doc.id;
 
-            if (docData.idUser === dataUser) {
-              return { ...docData, id };
-            } else {
-              return null;
-            }
-          })
-          .filter((item) => item !== null);
+          if (docData.idUser === dataUser) {
+            return { ...docData, id };
+          } else {
+            return null;
+          }
+        })
+        .filter((item) => item !== null);
 
-        if (data.length > 0) {
-          setUserData(data[0]);
-          fetchAllImages(data[0]); // Chamada para buscar as imagens passando o usuário como argumento
+      if (data.length > 0) {
+        setUserData(data[0]);
+        fetchAllImages(data[0]); // Chamada para buscar as imagens passando o usuário como argumento
+      }
+    });
+
+    const fetchAllImages = async (userData) => {
+      try {
+        const imagesRef = storage.ref();
+        const imagesSnapshot = await imagesRef.listAll();
+
+        const imageUrls = [];
+
+        for (const imageRef of imagesSnapshot.items) {
+          const downloadUrl = await imageRef.getDownloadURL();
+          imageUrls.push(downloadUrl);
         }
-      });
 
-      setIdUs(dataUser)
-      
-      const fetchAllImages = async (userData) => {
-        try {
-          const imagesRef = storage.ref();
-          const imagesSnapshot = await imagesRef.listAll();
+        const nameCover = userData.capa;
+        let coverUri = null;
+        const namePhoto = userData.foto;
+        let photoUri = null;
 
-          const imageUrls = [];
-
-          for (const imageRef of imagesSnapshot.items) {
-            const downloadUrl = await imageRef.getDownloadURL();
-            imageUrls.push(downloadUrl);
+        for (const imageUrl of imageUrls) {
+          const imageName = imageUrl.match(/\/o\/(.*?)\?alt/)[1];
+          if (imageName === nameCover) {
+            coverUri = imageUrl;
+            break;
           }
-
-          const nameCover = userData.capa;
-          let coverUri = null;
-          const namePhoto = userData.foto;
-          let photoUri = null;
-
-          for (const imageUrl of imageUrls) {
-            const imageName = imageUrl.match(/\/o\/(.*?)\?alt/)[1];
-            if (imageName === nameCover) {
-              coverUri = imageUrl;
-              break;
-            }
-          }
-
-          for (const imageUrl of imageUrls) {
-            const imageName = imageUrl.match(/\/o\/(.*?)\?alt/)[1];
-            if (imageName === namePhoto) {
-              photoUri = imageUrl;
-              break;
-            }
-          }
-
-          //console.log('URL da foto desejada:', photoUri, 'URL da capa:', coverUri);
-
-          setImageUrls({ photoUri, coverUri });
-        } catch (error) {
-          console.log('Erro ao buscar as imagens:', error);
-          setImageUrls({ photoUri: null, coverUri: null });
         }
-      };
 
-      return () => unsubscribe();
-
-    }, [dataUser]);
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView>
-          <View style={styles.coverContainer}>
-            {imageUrls.coverUri ? (
-              <Image source={{ uri: imageUrls.coverUri }} style={styles.cover} />
-            ) : 
-              <View style={styles.skeleton}></View>
-            }
-            {imageUrls.photoUri ? (
-              <Image source={{ uri: imageUrls.photoUri }} style={styles.profile} />
-            ) : 
-            <View style={styles.skeletonImage}></View>
+        for (const imageUrl of imageUrls) {
+          const imageName = imageUrl.match(/\/o\/(.*?)\?alt/)[1];
+          if (imageName === namePhoto) {
+            photoUri = imageUrl;
+            break;
           }
-            <TouchableOpacity style={styles.edit}><Ionicons name='create-outline' size={24} color="#1c3f7c"/></TouchableOpacity>
-          </View>
-          <View style={styles.containerContnet}>
-            <Text style={styles.Nome}>
-              {idUs.nome}
-            </Text>
+        }
+        setImageUrls({ photoUri, coverUri });
+      } catch (error) {
+        console.log('Erro ao buscar as imagens:', error);
+        setImageUrls({ photoUri: null, coverUri: null });
+      }
+    };
 
-            <View style={styles.containerInfoPlayer}>
-              <Text style={styles.label}>
-                Perfil do jogador
-              </Text>
-              <View style={styles.icons}>
-                <View style={styles.iconsRow}>
+    setIdUs(userData);
 
-                  <Text style={styles.icon}>
-                    <Ionicons
-                      name='location-outline'
-                      size={24}
-                      color="#1C3F7C"
-                      style={styles.iconSkills}
-                    />
-                    <Text style={styles.textIcon}> {idUs.cidade}
-                    </Text>
+    return () => unsubscribe();
+  }, [dataUser]);
 
-                    </Text>
-
-                  <Text style={styles.icon}>
-                    <Ionicons
-                      name='body-outline'
-                      size={24}
-                      color="#1C3F7C"
-                      style={styles.iconSkills}
-                    />
-                    <Text style={styles.textIcon}> 
-                    {idUs.altura}
-                    </Text>
-
-                    </Text>
-
-                  <Text style={styles.icon}>
-                    <Ionicons
-                      name='flag-outline'
-                      size={24}
-                      color="#1C3F7C"
-                      style={styles.iconSkills}
-                    />
-                    <Text style={styles.textIcon}> 
-                    {idUs.posicao}
-                    </Text>
-
-                    </Text>
-
-                </View>
-
-                <View style={styles.iconsRow}>
-
-                  <Text style={styles.icon}>
-                    <Ionicons
-                      name='person-outline'
-                      size={24}
-                      color="#1C3F7C"
-                      style={styles.iconSkills}
-                    />
-                    <Text style={styles.textIcon}> 
-                    {idUs.idade}
-                     anos</Text>
-
-                    </Text>
-
-                  <Text style={styles.icon}>
-                    <Ionicons
-                      name='barbell-outline'
-                      size={24}
-                      color="#1C3F7C"
-                      style={styles.iconSkills}
-                    />
-                    <Text style={styles.textIcon}>  
-                    {idUs.peso}
-                    kg</Text>
-
-                    </Text>
-
-                  <Text style={styles.icon}>
-                    <Ionicons
-                      name='walk-outline'
-                      size={24}
-                      color="#1C3F7C" 
-                      style={styles.iconSkills}
-                    />
-                    <Text style={styles.textIcon}>
-                         {idUs.perna}
-                    </Text>
-                    </Text>                    
-                </View>
-              </View>
-              <Text style={styles.label}>
-                Por onde passei
-              </Text>
-              <Text style={styles.textP}>
-                {idUs.passou}
-              </Text>
-              <Text style={styles.label}>
-                Sobre mim
-              </Text>
-              <Text style={styles.textP}>
-                {idUs.sobre}
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    )
+  function handleHeart(){
+    like? setLike(false) : setLike(true)
   }
 
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.coverContainer}>
+          {imageUrls.coverUri ? (
+            <Image source={{ uri: imageUrls.coverUri }} style={styles.cover} />
+          ) :
+            <View style={styles.skeleton}></View>
+          }
+          {imageUrls.photoUri ? (
+            <Image source={{ uri: imageUrls.photoUri }} style={styles.profile} />
+          ) :
+            <View style={styles.skeletonImage}></View>
+          }
+          <TouchableOpacity style={styles.edit} onPress={handleHeart}>
+            {like?
+            <AntDesign name="hearto" size={20} color="#1c3f7c" />
+            :
+            <AntDesign name="heart" size={20} color="#1c3f7c" />
+            }
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerContnet}>
+          <Text style={styles.Nome}>
+            {userData && userData.nome}
+          </Text>
+
+          <View style={styles.containerInfoPlayer}>
+            <Text style={styles.label}>
+              Perfil do jogador
+            </Text>
+            <View style={styles.icons}>
+              <View style={styles.iconsRow}>
+
+                <Text style={styles.icon}>
+                  <Ionicons
+                    name='location-outline'
+                    size={24}
+                    color="#1C3F7C"
+                    style={styles.iconSkills}
+                  />
+                  <Text style={styles.textIcon}> {userData && userData.cidade}
+                  </Text>
+
+                </Text>
+
+                <Text style={styles.icon}>
+                  <Ionicons
+                    name='body-outline'
+                    size={24}
+                    color="#1C3F7C"
+                    style={styles.iconSkills}
+                  />
+                  <Text style={styles.textIcon}>
+                    {userData && userData.altura}
+                  </Text>
+
+                </Text>
+
+                <Text style={styles.icon}>
+                  <Ionicons
+                    name='flag-outline'
+                    size={24}
+                    color="#1C3F7C"
+                    style={styles.iconSkills}
+                  />
+                  <Text style={styles.textIcon}>
+                    {userData && userData.posicao}
+                  </Text>
+
+                </Text>
+
+              </View>
+
+              <View style={styles.iconsRow}>
+
+                <Text style={styles.icon}>
+                  <Ionicons
+                    name='person-outline'
+                    size={24}
+                    color="#1C3F7C"
+                    style={styles.iconSkills}
+                  />
+                  <Text style={styles.textIcon}>
+                    {userData && userData.idade}
+                     anos</Text>
+
+                </Text>
+
+                <Text style={styles.icon}>
+                  <Ionicons
+                    name='barbell-outline'
+                    size={24}
+                    color="#1C3F7C"
+                    style={styles.iconSkills}
+                  />
+                  <Text style={styles.textIcon}>
+                    {userData && userData.peso}
+                    kg</Text>
+
+                </Text>
+
+                <Text style={styles.icon}>
+                  <Ionicons
+                    name='walk-outline'
+                    size={24}
+                    color="#1C3F7C"
+                    style={styles.iconSkills}
+                  />
+                  <Text style={styles.textIcon}>
+                    {userData && userData.perna}
+                  </Text>
+                </Text>
+              </View>
+            </View>
+            <Text style={styles.label}>
+              Por onde passei
+            </Text>
+            <Text style={styles.textP}>
+              {userData && userData.passou}
+            </Text>
+            <Text style={styles.label}>
+              Sobre mim
+            </Text>
+            <Text style={styles.textP}>
+              {userData && userData.sobre}
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HeaderBackButton } from "@react-navigation/elements";
+import { useNavigation } from "@react-navigation/native";
 
 import Splash from '../pages/Components/Splash'
 import Welcome from '../pages/Welcome'
@@ -17,72 +19,82 @@ import UserProfile from '../pages/Components/UserProfile'
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
-    return (
-        <Stack.Navigator>
-        <Stack.Screen
-            name="Splash"
-            component={Splash}
-            options={{ headerShown: false }}
-        />
-            <Stack.Screen
-                name="Welcome"
-                component={Welcome}
-                options={{ headerShown: false }}
+    const navigation = useNavigation(); 
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="LetsGo"
+        component={LetsGo}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NameUser"
+        component={NameUser}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PasswordUser"
+        component={PasswordUser}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SkillsUser"
+        component={SkillsUser}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DescritionUser"
+        component={DescritionUser}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PictureUser"
+        component={PictureUser}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="CoverUser"
+        component={CoverUser}
+        options={{ headerShown: false }}
+      /> 
+      <Stack.Screen
+        name="InfoUser"
+        component={InfoUser}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NewAccount"
+        component={NewAccount}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="UserProfile"
+        component={UserProfile}
+        options={() => ({
+          title: "Perfil do usuário",
+          headerLeft: () => (
+            <HeaderBackButton
+              onPress={() => navigation.navigate('TabRouter', { screen: 'Search' })}
+              tintColor="#1c3f7c"
             />
-            <Stack.Screen
-                name="LetsGo"
-                component={LetsGo}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="Login"
-                component={Login}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="NameUser"
-                component={NameUser}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="PasswordUser"
-                component={PasswordUser}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="SkillsUser"
-                component={SkillsUser}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="DescritionUser"
-                component={DescritionUser}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="PictureUser"
-                component={PictureUser}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="CoverUser"
-                component={CoverUser}
-                options={{ headerShown: false }}
-            /> 
-            <Stack.Screen
-                name="InfoUser"
-                component={InfoUser}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="NewAccount"
-                component={NewAccount}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="UserProfile"
-                component={UserProfile}
-            />
-        </Stack.Navigator>
-    )
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  )
 }
